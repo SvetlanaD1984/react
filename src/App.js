38ad6f5edff8d51d6e4ebcf3bd4878db7251b61f
svetlana <developer-svetlana@yandex.ru>
2022-02-03 17:49:21 +0500

    dz2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import { Message } from "./components/Message";
-
-
-const myText = "Hello!";
+import { Form } from "./components/Form";
+import { useEffect } from "react/cjs/react.development";
+import { AUTHORS } from "./utils/constants";
+import { MessageList } from "./components/MessageList";
 
 function App() {
-  const handleMessageClick = () => {
-    console.log("hello!");
+  const [messageList, setMessageList] = useState([]);
+
+  const handleAddMessage = (text) => {
+    sendMessage(text, AUTHORS.ME);
   };
+
+  const sendMessage = (text, author) => {
+    const newMsg = {
+      text,
+      author,
+    };
+    setMessageList((prevMessageList) => [...prevMessageList, newMsg]);
+  };
+
+  useEffect(() => {
+    let timeout;
+    if (messageList[messageList.length - 1]?.author === AUTHORS.ME) {
+      timeout = setTimeout(() => {
+        sendMessage("still here", AUTHORS.BOT);
+      }, 1000);
+    }
+
+    return () => clearTimeout(timeout);
+  }, [messageList]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        My First React App
-        </p>
-        
-        <Message
-          myString="my string"
-          text={myText}
-          onMessageClick={handleMessageClick}
-        />
-       
-      </header>
+      <div className="App-content">
+        <MessageList messages={messageList} />
+      </div>
+      <Form onSubmit={handleAddMessage} />
     </div>
   );
 }
