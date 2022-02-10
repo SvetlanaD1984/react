7ca84e634839e40d86f2f26eb0a364486c186dfe
svetlana <developer-svetlana@yandex.ru>
2022-02-10 17:25:59 +0500

    dz3nnew

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,13 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./styles.css";
 
-export class Message extends React.Component {
-  render() {
-    const { text, onMessageClick } = this.props;
-    return (
-      <h3 className="header" onClick={onMessageClick}>
-        Message: {text}
-      </h3>
-    );
-  }
-}
+export const Message = ({ text, author }) => {
+  return (
+    <div>
+      <span>
+        {author}: {text}
+      </span>
+    </div>
+  );
+};
+
+Message.propTypes = {
+  text: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.number]),
+  author: PropTypes.string.isRequired,
+};
