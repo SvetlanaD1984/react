8af33c4b3fcdb25ff0b70c60eb45e08b4f76f0b9
svetlana <developer-svetlana@yandex.ru>
2022-02-17 18:10:04 +0500

    dz6

---
+++ src/components/MessageList/index.js
@@ -0,0 +1,24 @@
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router";
+import { deleteMessage, editMessage } from "../../store/messages/actions";
+import { Message } from "../Message";
+
+export const MessageList = ({ messages }) => {
+  const { chatId } = useParams();
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(deleteMessage(chatId, id));
+  };
+
+  const handleEdit = (id) => {
+    dispatch(editMessage(chatId, id, "edited"));
+  };
+  return messages.map((message) => (
+    <div key={message.id}>
+      <Message text={message.text} author={message.author} />
+      <button onClick={() => handleDelete(message.id)}>Delete</button>
+      <button onClick={() => handleEdit(message.id)}>Edit</button>
+    </div>
+  ));
+};
