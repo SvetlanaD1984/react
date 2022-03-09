9669488297eb843d663ac1cdba1b060ff3466b22
svetlana <developer-svetlana@yandex.ru>
2022-03-09 23:54:49 +0500

    dz10

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
 import "./styles.css";
+import { ThemeContext } from "../../utils/ThemeContext";
 
-export class Message extends React.Component {
-  render() {
-    const { text, onMessageClick } = this.props;
-    return (
-      <h3 className="header" onClick={onMessageClick}>
-        Message: {text}
-      </h3>
-    );
-  }
-}
+export const Message = ({ text, author }) => {
+  return (
+    <div>
+      <span>
+        {author}: {text}
+      </span>
+    </div>
+  );
+};
